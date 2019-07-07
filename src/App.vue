19a6be37a6f01060175294a5d651184191d9d5dc
Haeyong/Kim <jinyrang79@hotmail.com>
2019-07-07 23:29:56 +0900

    Implement ViewPage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="app">
+    <tool-bar></tool-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToolBar from './components/ToolBar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ToolBar,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
 }
 </style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,17 +4,20 @@ import VueRouter from 'vue-router';
 import NewsView from '../vues/NewsView.vue';
 import JobsView from '../vues/JobsView.vue';
 import AskView from '../vues/AskView.vue';
+import UserView from '../vues/UserView.vue';
+import ItemView from '../vues/ItemView.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+    mode: 'history', // url 에 # 제거
     routes: [
         {
             path: '/',
             redirect: '/news'
-
         },
         {
+
             // url 주소    
             path: '/news',
             // url에 해당하는 컴포넌트 (page)
@@ -27,6 +30,14 @@ export const router = new VueRouter({
         {
             path: '/ask',
             component: AskView
+        },
+        {
+            path: '/user',
+            component: UserView
+        },
+        {
+            path: '/item',
+            component: ItemView
         },
     ]
 });
